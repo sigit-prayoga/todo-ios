270b9048b5f3a13ec3b4754cfc04e8f85e440a24
Sigit Prayoga <prayoga.sigit@gmail.com>
2017-03-07 09:30:09 +0700

    Initial Commit

---
+++ ToDo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ToDo
+//
+//  Created by Sigit Prayoga on 3/7/17.
+//  Copyright © 2017 Sigit Prayoga. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
